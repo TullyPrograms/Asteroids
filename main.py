9cb923c60c182f7c34d5194a00f2a66eb9ee3e0f
Skeldonson <tully.irving@gmail.com>
2025-12-18 10:54:23 -0500

    added tick timer

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 def main():
     print("Starting Asteroids with pygame version: 2.6.1")
     print("Screen width: 1280")
     print("Screen height: 720")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
